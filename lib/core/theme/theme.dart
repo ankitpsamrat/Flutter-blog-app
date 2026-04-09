3eadfdcb832c55503d5873b861a4146bdcfdc5d6
ankitpsamrat <ankit.p.samrat@gmail.com>
2026-04-09 22:49:43 +0530

    signup design completed

--- lib/core/theme/theme.dart
+++ lib/core/theme/theme.dart
@@ -2,7 +2,17 @@ import 'package:blog_app/core/theme/app_pallete.dart';
 import 'package:flutter/material.dart';
 
 class AppTheme {
+  static _border({Color color = AppPallete.borderColor}) => OutlineInputBorder(
+    borderRadius: BorderRadius.circular(10),
+    borderSide: BorderSide(width: 2, color: color),
+  );
+
   static final darkThemeMode = ThemeData.dark().copyWith(
     scaffoldBackgroundColor: AppPallete.backgroundColor,
+    inputDecorationTheme: InputDecorationTheme(
+      contentPadding: EdgeInsets.all(20),
+      enabledBorder: _border(),
+      focusedBorder: _border(color: AppPallete.gradient2),
+    ),
   );
 }
--- lib/features/auth/presentation/pages/signup_page.dart
+++ lib/features/auth/presentation/pages/signup_page.dart
@@ -1,3 +1,5 @@
+import 'package:blog_app/features/auth/presentation/widgets/auth_gradient_button.dart';
+import 'package:blog_app/features/auth/presentation/widgets/custom_text_field.dart';
 import 'package:flutter/material.dart';
 
 class SignupPage extends StatefulWidget {
@@ -8,8 +10,41 @@ class SignupPage extends StatefulWidget {
 }
 
 class _SignupPageState extends State<SignupPage> {
+  final _nameController = TextEditingController();
+  final _emailController = TextEditingController();
+  final _passwordController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: Padding(
+        padding: EdgeInsets.all(15),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text(
+              'Sign Up.',
+              style: TextStyle(fontSize: 50, fontWeight: FontWeight.bold),
+            ),
+            SizedBox(height: 30),
+            CustomTextField(hintText: 'Name', controller: _nameController),
+            SizedBox(height: 15),
+            CustomTextField(hintText: 'Email', controller: _emailController),
+            SizedBox(height: 15),
+            CustomTextField(
+              hintText: 'Password',
+              controller: _passwordController,
+              isObscureText: true,
+            ),
+            AuthGradientButton(
+              buttonText: 'Sign up',
+              onPressed: () {
+                // Handle sign up logic here
+              },
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
