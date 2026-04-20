c74d6c2f4cf0a8b29635ad3756a11024b026329d
ankitpsamrat <ankit.p.samrat@gmail.com>
2026-04-20 22:21:08 +0530

    implemented bloc

--- lib/features/auth/data/datasource/auth_remote_data_source.dart
+++ lib/features/auth/data/datasource/auth_remote_data_source.dart
@@ -1,3 +1,4 @@
+import 'package:blog_app/core/error/exception.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
 abstract interface class AuthRemoteDataSource {
@@ -16,7 +17,7 @@ abstract interface class AuthRemoteDataSource {
 class AuthRemoteDataSourceImpl implements AuthRemoteDataSource {
   final SupabaseClient supabaseClient;
 
-  AuthRemoteDataSourceImpl({required this.supabaseClient});
+  const AuthRemoteDataSourceImpl({required this.supabaseClient});
 
   @override
   Future<String> signInWithEmailPassword({
@@ -32,8 +33,21 @@ class AuthRemoteDataSourceImpl implements AuthRemoteDataSource {
     required String name,
     required String email,
     required String password,
-  }) {
-    // TODO: implement signUpWithEmailPassword
-    throw UnimplementedError();
+  }) async {
+    try {
+      final response = await supabaseClient.auth.signUp(
+        email: email,
+        password: password,
+        data: {'name': name},
+      );
+
+      if (response.user == null) {
+        throw const ServerException('User registration failed');
+      }
+
+      return response.user!.id;
+    } catch (e) {
+      throw ServerException(e.toString());
+    }
   }
 }
---
+++ lib/features/auth/data/repository/auth_repository_impl.dart
@@ -0,0 +1,39 @@
+import 'package:blog_app/core/error/exception.dart';
+import 'package:blog_app/core/error/failure.dart';
+import 'package:blog_app/features/auth/data/datasource/auth_remote_data_source.dart';
+import 'package:blog_app/features/auth/domain/repository/auth_repository.dart';
+import 'package:fpdart/fpdart.dart';
+
+class AuthRepositoryImpl implements AuthRepository {
+  final AuthRemoteDataSource remoteDataSource;
+
+  const AuthRepositoryImpl({required this.remoteDataSource});
+
+  @override
+  Future<Either<Failure, String>> signInWithEmailPassword({
+    required String email,
+    required String password,
+  }) {
+    // TODO: implement signInWithEmailPassword
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<Either<Failure, String>> signUpWithEmailPassword({
+    required String name,
+    required String email,
+    required String password,
+  }) async {
+    try {
+      final String userId = await remoteDataSource.signUpWithEmailPassword(
+        name: name,
+        email: email,
+        password: password,
+      );
+
+      return Right(userId);
+    } on ServerException catch (e) {
+      return Left(Failure(message: e.message));
+    }
+  }
+}
